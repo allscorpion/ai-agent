d20bca4be0ca404a184fa294875e49909973c111
allscorpion <deadlyryz@hotmail.co.uk>
2025-09-15 14:11:28 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+import sys 
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+if (len(sys.argv) <= 1):
+    print("make sure to provide a prompt")
+    sys.exit(1)
+
+def main():
+    user_prompt = sys.argv[1]
+    is_verbose = "--verbose" in sys.argv
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+
+    if (is_verbose):
+        print(f"User prompt: {user_prompt}")
+    print(response.text)
+    if is_verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+if __name__ == "__main__":
+    main()
