3bfa1220db13291c18003a26426862fa68b722b5
allscorpion <deadlyryz@hotmail.co.uk>
2025-09-16 22:00:05 +0100

    calling functions

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import os
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.run_python_file import schema_run_python_file
-from functions.write_file import schema_write_file
+from functions.get_files_info import get_files_info, schema_get_files_info
+from functions.get_file_content import get_file_content, schema_get_file_content
+from functions.run_python_file import run_python_file, schema_run_python_file
+from functions.write_file import write_file, schema_write_file
 import sys 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -14,6 +14,44 @@ client = genai.Client(api_key=api_key)
 if (len(sys.argv) <= 1):
     print("make sure to provide a prompt")
     sys.exit(1)
+
+def call_function(function_call_part, verbose=False):
+    if (verbose):
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f"Calling function: {function_call_part.name}")
+    result = None
+    match (function_call_part.name):
+        case "get_file_content":
+            result = get_file_content("./calculator", **function_call_part.args)
+        case "get_files_info":
+            result = get_files_info("./calculator", **function_call_part.args)
+        case "run_python_file":
+            result = run_python_file("./calculator", **function_call_part.args)
+        case "write_file":
+            result = write_file("./calculator", **function_call_part.args)
+
+    if result == None:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_call_part.name,
+                    response={"error": f"Unknown function: {function_call_part.name}"},
+                )
+            ],
+        )
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_call_part.name,
+                response={"result": result},
+            )
+        ],
+    )
+            
+
 
 def main():
     system_prompt = """
@@ -58,7 +96,17 @@ def main():
 
     if response.function_calls:
         for function_call_part in response.function_calls:
-            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+            func_response = call_function(function_call_part, verbose=is_verbose)
+            if func_response.parts and func_response.parts[0].function_response:
+                response = func_response.parts[0].function_response.response
+
+                if not response:
+                    raise Exception("no function response returned")
+                elif is_verbose:
+                    print(f"-> {response}")
+
+            else:
+                raise Exception("no function response returned")
     else:
         print(response.text)
         
